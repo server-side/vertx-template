a1299e621304d49d5963de97df0633076d011258
zhaopumi <zhaopuming@gmail.com>
2014-06-11 16:22:17 +0800

    Change url to /server-side/vertx-template

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject vertx-template/lein-template "0.1.4-RELEASE"
-  :description "lein template for vertx development in maven"
-  :url "http://github.com/zhaopuming/vertx-template"
+(defproject vertx-template/lein-template "0.1.5-RELEASE"
+  :description "Lein template for vertx development with maven support"
+  :url "http://github.com/server-side/vertx-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
